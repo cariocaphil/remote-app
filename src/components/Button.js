49dfa46849a46aee972c0a6d21d7bacfe3f92f47
cariocaphil <cariocaphil@gmail.com>
2025-01-26 23:54:01 +0100

    add customization via props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Button = () => {
-  return <button>I am a Remote Button!</button>;
+const Button = ({ accountId }) => {
+  const accountInfo = {
+    "12345": "This is account 12345's information.",
+    "67890": "This is account 67890's information.",
+    "11121": "This is account 11121's information.",
+  };
+
+  return (
+    <div>
+        <h1>Remote App (Micro-Frontend)</h1>
+        <button>
+        {accountInfo[accountId] || "Select a valid account ID to see information."}
+        </button>
+    </div>
+  );
 };
 
-export default Button;
+export default Button
